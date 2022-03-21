c136f773eb9217fda09083a0e69dc6af9a7af1f2
MarufPulok <53114706+MarufPulok@users.noreply.github.com>
2022-03-21 13:20:02 +0600

    Add files via upload

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", function () {
     var buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
   });
 
   document.addEventListener("keydown", function (event) {
     makeSound(event.key);
+    buttonAnimation(event.key);
   });
 
   //detecting keyBoard press
@@ -44,7 +46,16 @@ for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
         console.log("error");
     }
   }
-}
+
+
+  function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+    setTimeout(function(){
+      activeButton.classList.remove("pressed");
+    },100);
+  }
+} 
 
 // var audio = new Audio("sounds/tom-4.mp3");
 // audio.play();
